7aabec8542f30b586a7f07a0b703b825e7380843
Sean Smith <40581796+smithlabs@users.noreply.github.com>
2020-11-14 06:08:23 -0700

    Matches examples directory with the changes made to the rest of the repo.

--- examples/complete/user-data.sh
+++ examples/complete/user-data.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
+# ------------------------------------------------------------------
+# [Sean Smith (smithlabs)] - Run a 'Hello, World! Go web server 
+#          Description
+#
+#          This script downloads and runs a pre-compiled
+#          Go web server that prints 'Hello, World!' 
+# ------------------------------------------------------------------
 
+# ---------------------------------------------------------------------------------------------------------------------
+# Download the Go web server binary
+# Make it executable
+# ---------------------------------------------------------------------------------------------------------------------
 curl -LJO https://github.com/smithlabs/go-example-servers/raw/main/01-hello-world/bin/main
 chmod +x main
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Run the server
+# nohup catches the hangup signal and the ampersand runs the process in the background.
+# ---------------------------------------------------------------------------------------------------------------------
 nohup ./main &
