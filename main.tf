704c6adeb5ff1cf3cc1a8fb650bedd9e849aca32
Sean Smith <40581796+smithlabs@users.noreply.github.com>
2020-11-16 16:01:23 -0700

    Ran terraform fmt on code because it failed terraform fmt -check in GitHub actions

--- main.tf
+++ main.tf
@@ -23,22 +23,22 @@ provider "aws" {
 module "elb" {
   source = "github.com/smithlabs/terraform-aws-elb?ref=v1.0.0"
 
-  elb_name           = var.name
-  subnet_ids         = data.aws_subnet_ids.default.ids
+  elb_name   = var.name
+  subnet_ids = data.aws_subnet_ids.default.ids
 }
 
 module "asg" {
   source = "github.com/smithlabs/terraform-aws-asg-rolling-deploy?ref=v1.0.0"
 
-  ami                = "ami-02354e95b39ca8dec" # Amazon Linux
-  instance_type      = "t2.micro"
-  name               = var.name
-  environment        = var.environment
-  user_data          = data.template_file.user_data.rendered
-  min_size           = 2
-  max_size           = 2
-  subnet_ids         = data.aws_subnet_ids.default.ids
-  load_balancers     = [module.elb.elb_name]
+  ami            = "ami-02354e95b39ca8dec" # Amazon Linux
+  instance_type  = "t2.micro"
+  name           = var.name
+  environment    = var.environment
+  user_data      = data.template_file.user_data.rendered
+  min_size       = 2
+  max_size       = 2
+  subnet_ids     = data.aws_subnet_ids.default.ids
+  load_balancers = [module.elb.elb_name]
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -50,20 +50,20 @@ resource "aws_security_group_rule" "allow_elb_http_inbound" {
   type              = "ingress"
   security_group_id = module.elb.elb_security_group_id
 
-  from_port         = local.http_port
-  to_port           = local.http_port
-  protocol          = local.tcp_protocol
-  cidr_blocks       = local.all_ips
+  from_port   = local.http_port
+  to_port     = local.http_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ips
 }
 
 resource "aws_security_group_rule" "allow_elb_all_outbound" {
   type              = "egress"
   security_group_id = module.elb.elb_security_group_id
 
-  from_port         = local.any_port
-  to_port           = local.any_port
-  protocol          = local.any_protocol
-  cidr_blocks       = local.all_ips
+  from_port   = local.any_port
+  to_port     = local.any_port
+  protocol    = local.any_protocol
+  cidr_blocks = local.all_ips
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -76,21 +76,21 @@ resource "aws_security_group_rule" "allow_server_http_inbound" {
   type              = "ingress"
   security_group_id = module.asg.instance_security_group_id
 
-  from_port         = var.server_port
-  to_port           = var.server_port
-  protocol          = local.tcp_protocol
-  cidr_blocks       = local.all_ips
+  from_port   = var.server_port
+  to_port     = var.server_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ips
 }
 
 # Allow the EC2 instance to reach the Internet to perform the server setup in user-data.sh
 resource "aws_security_group_rule" "allow_server_all_outbound" {
-  type		     = "egress"
-  security_group_id  = module.asg.instance_security_group_id
+  type              = "egress"
+  security_group_id = module.asg.instance_security_group_id
 
-  from_port	     = local.any_port
-  to_port	     = local.any_port
-  protocol	     = local.any_protocol
-  cidr_blocks	     = local.all_ips
+  from_port   = local.any_port
+  to_port     = local.any_port
+  protocol    = local.any_protocol
+  cidr_blocks = local.all_ips
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -98,11 +98,11 @@ resource "aws_security_group_rule" "allow_server_all_outbound" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 locals {
-  http_port           = 80
-  any_port            =  0
-  any_protocol        = "-1"
-  tcp_protocol        = "tcp"
-  all_ips             = ["0.0.0.0/0"]
+  http_port    = 80
+  any_port     = 0
+  any_protocol = "-1"
+  tcp_protocol = "tcp"
+  all_ips      = ["0.0.0.0/0"]
 }
 
 data "template_file" "user_data" {
